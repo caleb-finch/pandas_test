15898ec0113d560318f68c602c2feb69a1727cf9
Caleb Finch <36723305+caleb-finch@users.noreply.github.com>
2026-01-08 11:34:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 ##test the pandas import and see if i can import a csv, get the list, change the contents of the list, and save the csv
 
-import pandas 
+import pandas for pd
+
+def main()
+  df = pd.read_csv('database.csv')
+  print(df)
+
+main()
