b8414efad5519046c81071cd49dc3f37ed3689c8
Caleb Finch <36723305+caleb-finch@users.noreply.github.com>
2026-01-10 06:08:02 +0000

    this should work in my bank project

--- main.py
+++ main.py
@@ -3,34 +3,23 @@
 import pandas as pd
 
 def main():
-  df = pd.read_csv('database.csv')
+  df = pd.read_csv('database.csv',index_col=None)
   df_as_a_list = df.values.tolist()
+  print(df)
+
   for x in df_as_a_list:
-    print(x[0])
-    if x[0] == 'ljames':
-       username = x[0]
-       break
-  fullList = x
-  print(fullList)
-  print(username)
-      
-
-    
-    #print(df_as_a_list(x))
-
-  #print(df)
-  #print(df.loc[:, 'username'])
-  #usernameList = df.loc[:, 'username']
-  #print(usernameList)
-  #for x in df.loc[:, 'username']:
-  #  if x=='ljames':
-  #    print('true! Username is {x}')
-
-
-
-  #print(df.shape)
-  #[x,y] = df.shape
-  #print(x)
-  #print(y)
+    if x[0] == 'cfinch':
+      username = x[0]
+      savingsAmnt = x[1]
+      checkingAmnt = x[2]
+      print(checkingAmnt)
+      transsavingsAmnt = x[1] + 69
+      transcheckingAmnt = x[2] + 420
+      print(transcheckingAmnt)
+      break
+  df=df.replace({'savingsAmnt': savingsAmnt}, transsavingsAmnt)
+  df=df.replace({'checkingAmnt': checkingAmnt}, transcheckingAmnt)
+  print(df)
+  df.to_csv('database.csv', sep=',', index=False)
 
 main()
